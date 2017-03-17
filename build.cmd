76659895d2bdaa44f1011c8b7056d7672e329a69
Joe <qiaofei32@yahoo.com>
2017-03-17 12:29:27 +0800

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,6 +1,11 @@
 @echo off
+
+set path=C:\Windows\Microsoft.NET\Framework64\v3.5
+
 if exist "%ProgramFiles%\Fiddler2\Fiddler.exe" set "FIDDLERPATH=%ProgramFiles%\Fiddler2"
+
 if exist "%ProgramFiles(x86)%\Fiddler2\Fiddler.exe" set "FIDDLERPATH=%ProgramFiles(x86)%\Fiddler2"
+
 if not defined FIDDLERPATH echo Can't find Fiddler in Program Files & exit
 
 csc /d:TRACE /target:library /out:"%USERPROFILE%\Documents\fiddler2\scripts\AutoCapture.dll" AutoCapture.cs Win32File.cs /reference:"%FIDDLERPATH%\Fiddler.exe"
